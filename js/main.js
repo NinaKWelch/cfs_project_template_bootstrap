baaa7eacf11e9ca67af730cd8b709e4e655f0007
NinaW <35150414+NinaKWelch@users.noreply.github.com>
2020-05-15 16:28:02 +0100

    Remove duplicate

--- js/main.js
+++ js/main.js
@@ -1,10 +1,6 @@
-<<<<<<< HEAD
 /**
 * GLOBAL JAVASCRIPT
 */
-
-
-/* Global */
 
 function search() {
     prompt("What are you looking for?")
@@ -21,24 +17,3 @@ function submitForm() {
 function requestPassword() {
     prompt("Tell us your email")
 }
-=======
-/* scripts */
-
-function toggleMenu() {
-    const button = document.getElementById("toggle-button");
-    const menu = document.getElementById("toggle-menu");
-
-    if (menu.style.display === "block") {
-        menu.style.display = "none";
-        button.innerHTML = `<i class="fas fa-bars"></i>`;
-      } else {
-        menu.style.display = "block";
-        button.innerHTML = `<i class="fas fa-times"></i>`;
-      };
-
-};
-
-function search() {
-    // TODO
-}
->>>>>>> 2033c2e8406321de1b68e64e865251dc792dcf44
